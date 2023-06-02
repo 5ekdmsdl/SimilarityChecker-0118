bdf8bb8840bd8b975861c717325bc10020b557c1
dawn <5ekdmsdldl@gmail.com>
2023-06-02 15:19:51 +0900

    Full commit

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -3,7 +3,27 @@
 #include <gtest/gtest.h>
 #include "../BestReview/SimilarityChecker.cpp"
 
-TEST(SimilarityChecker, Test)
+TEST(SimilarityChecker, InvalidInput_SmallLetter)
 {
-	EXPECT_EQ(1, 1);
+	SimilarityChecker check;
+	EXPECT_THROW(check.getAlphaScore("abc", "BC") , invalid_argument);
+	EXPECT_THROW(check.getAlphaScore("BC", "abc") , invalid_argument);
+}
+
+TEST(SimilarityChecker, AllDiff)
+{
+	SimilarityChecker check;
+	EXPECT_EQ(check.getAlphaScore("ABC", "EFG") , 0);
+}
+
+TEST(SimilarityChecker, MAX_SCORE)
+{
+	SimilarityChecker check;
+	EXPECT_EQ(check.getAlphaScore("AAABB", "AB"), 40);
+}
+
+TEST(SimilarityChecker, partial_SCORE)
+{
+	SimilarityChecker check;
+	EXPECT_EQ(check.getAlphaScore("AA", "AAE") , 20);
 }
