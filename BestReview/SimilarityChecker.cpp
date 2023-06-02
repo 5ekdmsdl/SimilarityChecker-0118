e0d9d5fa760f0bfd85503f8b46e74de970af5629
dawn <5ekdmsdldl@gmail.com>
2023-06-02 11:50:28 +0900

    Special cases : 0 or MAX score

--- BestReview/SimilarityChecker.cpp
+++ BestReview/SimilarityChecker.cpp
@@ -1,4 +1,18 @@
+#include <string>
+using namespace std;
+
 class SimilarityChecker
 {
 public:
+	explicit SimilarityChecker(const string answer) : answer(answer) {};
+	int getScore1(string guess);
+
+	string answer;
 };
+
+int SimilarityChecker::getScore1(string guess)
+{
+	if (answer.length() == guess.length()) return 60;
+	return 0;
+}
+
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -3,7 +3,21 @@
 #include <gtest/gtest.h>
 #include "../BestReview/SimilarityChecker.cpp"
 
-TEST(SimilarityChecker, Test)
+TEST(SimilarityChecker, LengthDoubleLonger)
 {
-	EXPECT_EQ(1, 1);
+	SimilarityChecker checker("A");
+	EXPECT_EQ(0, checker.getScore1("BB"));
 }
+
+TEST(SimilarityChecker, PerfectMatch)
+{
+	SimilarityChecker checker("ASD");
+	EXPECT_EQ(60, checker.getScore1("DSA"));
+}
+
+/*
+TEST(SimilarityChecker, PartialScore1)
+{
+	SimilarityChecker checker("AAABB");
+	EXPECT_EQ(60, checker.getScore1("BAA"));
+}*/
