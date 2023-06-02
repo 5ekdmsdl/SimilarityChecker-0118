91acb6813d2f4feaf19b6580f962289e6397eb78
dawn <5ekdmsdldl@gmail.com>
2023-06-02 13:44:28 +0900

    Refactoring] general score calculation

--- BestReview/SimilarityChecker.cpp
+++ BestReview/SimilarityChecker.cpp
@@ -7,30 +7,46 @@ public:
 	explicit SimilarityChecker(const string answer) : answer(answer) {};
 	float getScore1(string guess);
 
+private:
+	bool IsMAXScore(string guess);
+	bool IsZeroScore(string guess);
+	float calculateWithFormula(int longerLength, int shorterLength);
+	float CalculateScore(string guess);
+
+private:
 	string answer;
+	const int MAX_SCORE = 60;
 };
 
 float SimilarityChecker::getScore1(string guess)
 {
-	if (answer.length() == guess.length()) return 60;
+	if (IsMAXScore(guess)) return MAX_SCORE;
+	if (IsZeroScore(guess)) return 0;
 
-	int longLength = 0;
-	int shortLength = 0;
-	if(answer.length() > guess.length())
-	{
-		longLength = answer.length();
-		shortLength = guess.length();
-	}
-	else
-	{
-		longLength = guess.length();
-		shortLength = answer.length();
-	}
-	if (longLength > shortLength * 2) return 0;
-
-	float score = (60 - (longLength - shortLength) * (60 / (float) shortLength)) ;
-	return score;
-
-	return 0;
+	return CalculateScore(guess);
 }
 
+bool SimilarityChecker::IsMAXScore(string guess)
+{
+	return answer.length() == guess.length();
+}
+
+bool SimilarityChecker::IsZeroScore(string guess)
+{
+	int longerLength = (answer.length() > guess.length()) ? answer.length() : guess.length();
+	int shorterLength = (answer.length() > guess.length()) ? guess.length() : answer.length();
+	if (longerLength > shorterLength * 2) return true;
+	return false;
+}
+
+float SimilarityChecker::CalculateScore(string guess)
+{
+	int longerLength = (answer.length() > guess.length()) ? answer.length() : guess.length();
+	int shorterLength = (answer.length() > guess.length()) ? guess.length() : answer.length();
+	return calculateWithFormula(longerLength, shorterLength);
+}
+
+float SimilarityChecker::calculateWithFormula(int longerLength, int shorterLength)
+{
+	return MAX_SCORE - (longerLength - shorterLength) * (MAX_SCORE / (float)shorterLength);
+}
